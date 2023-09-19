6ec861aee844e8d04094be57dfe57b009a593d8f
Mert Alver <104023146+mertalver@users.noreply.github.com>
2023-09-19 12:20:16 +0300

    Add files via upload

---
+++ RegressionDataSets/Communities and Crime/main.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+
+dataset = pd.read_csv("communities.data", header=None, na_values="?")
+X = dataset.iloc[:, 5:-1]
+y = dataset.iloc[:, -1].values
+
+X = X.fillna(X.mean())
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+regressor = RandomForestRegressor(n_estimators=35, random_state=0)
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+
+cv_score = cross_val_score(regressor, X_train, y_train, cv=10)
+
+print(r2_score(y_test, y_pred))
+print(np.sqrt(mean_squared_error(y_test, y_pred)))
+print(mean_absolute_error(y_test, y_pred))
+print(np.mean(cv_score))
