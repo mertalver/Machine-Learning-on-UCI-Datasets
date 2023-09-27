661f676dbe1b4ef7ea9dfa87d99ada222304faad
Mert Alver <104023146+mertalver@users.noreply.github.com>
2023-09-27 12:45:39 +0300

    Add files via upload

---
+++ ClassificationDataSets/Internet Advertisements/main.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+
+dataset = pd.read_csv("ad.data", header=None, na_values="?")
+dataset = dataset.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+dataset.replace("?", np.nan, inplace=True)
+dataset.dropna(axis=0, inplace=True)
+
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+classifier = RandomForestClassifier(n_estimators=20, criterion="gini", random_state=0)
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+cv_score = cross_val_score(classifier, X_train, y_train, cv=10)
+
+print(classification_report(y_test, y_pred))
+print(confusion_matrix(y_test, y_pred))
+print(accuracy_score(y_test, y_pred))
+print(np.mean(cv_score))
