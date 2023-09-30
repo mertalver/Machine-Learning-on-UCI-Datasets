02b27392a09e144a7f9bdfe00f34ca41667f2989
Mert Alver <104023146+mertalver@users.noreply.github.com>
2023-09-30 17:53:33 +0300

    Add files via upload

---
+++ RegressionDataSets/Gas Turbine CO and NOx Emission Dataset/main_gt2012.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+
+dataset = pd.read_csv("gt_2012.csv")
+X = dataset.iloc[:, :-2].values
+y = dataset.iloc[:, -2:].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+poly_reg = PolynomialFeatures(degree=3)
+X_train = poly_reg.fit_transform(X_train)
+X_test = poly_reg.transform(X_test)
+
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+
+cv_score = cross_val_score(regressor, X_train, y_train, cv=10)
+
+print(r2_score(y_test, y_pred))
+print(np.sqrt(mean_squared_error(y_test, y_pred)))
+print(mean_absolute_error(y_test, y_pred))
+print(np.mean(cv_score))
+
---
+++ RegressionDataSets/Gas Turbine CO and NOx Emission Dataset/main_gt2013.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+
+dataset = pd.read_csv("gt_2013.csv")
+X = dataset.iloc[:, :-2].values
+y = dataset.iloc[:, -2:].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+regressor = RandomForestRegressor(n_estimators=20, random_state=0)
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+
+cv_score = cross_val_score(regressor, X_train, y_train, cv=10)
+
+print(r2_score(y_test, y_pred))
+print(np.sqrt(mean_squared_error(y_test, y_pred)))
+print(mean_absolute_error(y_test, y_pred))
+print(np.mean(cv_score))
