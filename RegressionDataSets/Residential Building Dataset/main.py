7b6a47439d63a259444c72a1f7cf82a775c8fd2a
Mert Alver <104023146+mertalver@users.noreply.github.com>
2023-10-02 22:57:43 +0300

    Add files via upload

---
+++ RegressionDataSets/Residential Building Dataset/main.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+
+dataset = pd.read_csv("Residential-Building-Data-Set.csv", sep=";", decimal=",")
+dataset.columns = dataset.columns.str.replace(";", " ")
+X = dataset.drop(["V-9", "V-10"], axis=1)
+y = dataset[["V-9", "V-10"]]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+
+cv_score = cross_val_score(regressor, X_train, y_train, cv=10)
+
+print(r2_score(y_test, y_pred))
+print(np.sqrt(mean_squared_error(y_test, y_pred)))
+print(mean_absolute_error(y_test, y_pred))
+print(np.mean(cv_score))
