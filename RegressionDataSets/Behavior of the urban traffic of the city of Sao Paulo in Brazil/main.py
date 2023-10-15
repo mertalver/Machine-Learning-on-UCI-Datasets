474a5f4d692e956ff7a2490592adf80d12908e9a
Mert Alver <104023146+mertalver@users.noreply.github.com>
2023-10-15 20:59:25 +0300

    Add files via upload

---
+++ RegressionDataSets/Behavior of the urban traffic of the city of Sao Paulo in Brazil/main.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+
+dataset = pd.read_csv("Behavior of the urban traffic of the city of Sao Paulo in Brazil.csv", sep=";", decimal=",")
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+
+cv_score = cross_val_score(regressor, X_train, y_train, cv=10)
+
+print(r2_score(y_test, y_pred))
+print(np.sqrt(mean_squared_error(y_test, y_pred)))
+print(mean_absolute_error(y_test, y_pred))
+print(np.mean(cv_score))
